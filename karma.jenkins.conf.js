91db7ad2534b1838f786add217170c686f5c8010
Naufal Fikri <nauffik@gmail.com>
2019-11-04 21:42:52 +1100

    WIP: FIX Wrong Chromium instance being called on unit tests

--- karma.jenkins.conf.js
+++ karma.jenkins.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeNoSandbox'],
     customLaunchers: {
       ChromeNoSandbox: {
         base: 'ChromeHeadless',
