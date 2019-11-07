f2abd63ce889e44af3dd81c1c9c32559a076a7ca
Naufal Fikri <nauffik@gmail.com>
2019-11-07 21:31:24 +1100

    Fixed unit testing not importing the correct modules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { SplashPageComponent } from './splashpage/splashpage.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,7 +12,8 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        SplashPageComponent
+        SplashPageComponent,
+        HeaderComponent
       ],
     }).compileComponents();
   }));
--- src/app/resume/resume.component.spec.ts
+++ src/app/resume/resume.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResumeComponent } from './resume.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ResumeComponent', () => {
   let component: ResumeComponent;
@@ -8,7 +9,8 @@ describe('ResumeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResumeComponent ]
+      declarations: [ ResumeComponent ],
+      imports: [ RouterTestingModule ],
     })
     .compileComponents();
   }));
