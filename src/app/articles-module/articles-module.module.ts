58aa9c69eea1859409dc2f5350da0a0fd492ca87
Naufal Fikri <nauffik@gmail.com>
2019-11-08 17:07:09 +1100

    Added extra modules for lazy loading

---
+++ src/app/articles-module/articles-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArticlesModuleRoutingModule } from './articles-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArticlesModuleRoutingModule
+  ]
+})
+export class ArticlesModuleModule { }
---
+++ src/app/projects-module/projects-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectsModuleRoutingModule } from './projects-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProjectsModuleRoutingModule
+  ]
+})
+export class ProjectsModuleModule { }
