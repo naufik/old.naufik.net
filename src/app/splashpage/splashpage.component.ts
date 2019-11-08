5b458ecf53c12f749f386c820775e49f86fb7456
Naufal Fikri <nauffik@gmail.com>
2019-11-08 16:56:35 +1100

    added (temp) navbar

--- src/app/splashpage/splashpage.component.ts
+++ src/app/splashpage/splashpage.component.ts
@@ -8,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class SplashPageComponent implements OnInit {
 
   myName = "Naufal Fikri";
+  displayCopyMessage = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  copyEmail() {
+    if (!this.displayCopyMessage) {
+      const createCopy = (e: ClipboardEvent) => {
+        e.clipboardData.setData('text/plain', 'naufal@naufik.net');
+        e.preventDefault();
+      };
+
+      document.addEventListener('copy', createCopy);
+      document.execCommand('copy');
+      document.removeEventListener('copy', createCopy);
+      this.displayCopyMessage = true;
+
+      setTimeout(() => {
+        this.displayCopyMessage = false;
+      }, 1200);
+    }
   }
 
 
