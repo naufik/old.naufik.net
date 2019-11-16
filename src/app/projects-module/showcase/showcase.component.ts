58dc1b6d2d2c8d881fd2c5fb1908d282ff55eff6
Naufal Fikri <nauffik@gmail.com>
2019-11-16 17:28:22 +1100

    added showcase element

--- src/app/projects-module/showcase/showcase.component.ts
+++ src/app/projects-module/showcase/showcase.component.ts
@@ -9,18 +9,40 @@ export class ShowcaseComponent implements OnInit {
 
   dummy = [
     {
-      title: 'THIS website',
-      url: 'http://naufik.net',
-      description: 'Yes, this, very website',
+      title: 'This Website',
+      url: 'naufik-net-website',
+      description: 'Yes, this, very website built using Angular and APIs.',
+      tags: ['angular', 'portfolio', 'web']
+    },
+    {
+      title: 'GitBlogger',
+      url: 'git-blogger',
+      description: 'An automation script to allow me to publish Blog posts only via Git Push.'
     },
     {
       title: 'Shadow Blocks',
-      url: 'https://github.com/naufik/swen20003-shadow-blocks',
+      url: 'shadow-blocks',
       description: 'I am super proud of this one!'
     },
     {
       title: 'We\'ll Figure it Out',
+      url: 'wfio-dont-lose-your-way',
       description: 'Gets elders from point A to point B.'
+    },
+    {
+      title: 'PlayTime',
+      url: 'playtime-dp-proj',
+      description: '(Obsolete) C# app to control play time.'
+    },
+    {
+      title: 'TripleZero',
+      url: 'triple-zero',
+      description: 'From Melbourne Games Week Hackathon! An Alexa-powered emergency dispatcher game.'
+    },
+    {
+      title: 'Bills -> Wise',
+      url: 'bills-arrow-wise',
+      description: 'Lambda-powered automation script to automatically bill my roommate when a bill arrives via email.'
     }
   ];
 
